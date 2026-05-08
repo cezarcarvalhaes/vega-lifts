93492afe83384a590d2ea917d6d847ee5c176f02
Cezar Carvalhaes <cezarcarvalhaes@gmail.com>
2026-05-08 16:19:31 -0400

    chore: lint

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -1,5 +1,5 @@
+const path = require('node:path')
 const { getDefaultConfig } = require('expo/metro-config')
-const path = require('path')
 
 const projectRoot = __dirname
 const workspaceRoot = path.resolve(projectRoot, '../..')
